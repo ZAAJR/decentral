20d7f73c525942e04830a763e935ead97ec8fdf9
ZAAJR <allan@ridley.za.net>
2024-03-14 16:48:32 +0200

    Create tailscale_setup.sh

---
+++ tailscale_setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo PLease enter the Tailscale 4via6 subnet translator ID
+read translator_id
+
+sudo apt-get install apt-transport-https
+curl -fsSL https://pkgs.tailscale.com/stable/raspbian/bullseye.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg > /dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/raspbian/bullseye.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+sudo apt-get update
+sudo apt-get install tailscale
+sudo tailscale up
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+routes="'tailscale debug via $translator_id 192.168.1.0/24'"
+tailscale up --advertise-routes=$routes
