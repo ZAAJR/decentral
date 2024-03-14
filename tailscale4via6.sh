63977949b89845f8ee94e7e3f62871cdca73673b
ZAAJR <allan@ridley.za.net>
2024-03-14 16:57:19 +0200

    Update tailscale4via6.sh

--- tailscale4via6.sh
+++ tailscale4via6.sh
@@ -3,7 +3,7 @@
 echo PLease enter the Tailscale 4via6 subnet translator ID
 read translator_id
 
-routes="'tailscale debug via $translator_id 192.168.1.0/24'"
+tailscale debug via $translator_id 192.168.1.0/24
 echo $routes
 
 tailscale up --advertise-routes=$routes
